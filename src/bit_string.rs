a057abee6c442ddb02cea67f1518a54218b79a91
Michael Neumann <mneumann@ntecs.de>
2015-10-23 13:34:06 +0200

    Implement BitString

---
+++ src/bit_string.rs
@@ -0,0 +1,68 @@
+use super::bit_vec::BitVec;
+use std::usize;
+
+pub trait BitRepr {
+    fn nbits(&self) -> usize;
+    fn get_bit(&self, pos: usize) -> bool;
+}
+
+impl BitRepr for usize {
+    fn nbits(&self) -> usize { usize::BITS }
+    fn get_bit(&self, pos: usize) -> bool {
+        assert!(pos < self.nbits());
+        (*self >> pos) & 1 == 1
+    }
+}
+
+impl BitRepr for bool {
+    fn nbits(&self) -> usize { 1 }
+    fn get_bit(&self, pos: usize) -> bool {
+        assert!(pos == 0);
+        *self
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct BitString {
+    bits: BitVec,
+}
+
+impl BitString {
+    /// Append the lowest `nbits` bits of `value` to the BitString, msb first.
+    pub fn append_bits_msb_from<T:BitRepr>(&mut self, value: &T, nbits: usize) {
+        assert!(nbits <= value.nbits());
+        self.bits.reserve(nbits);
+        for i in (0 .. nbits).rev() {
+            self.bits.push(value.get_bit(i));
+        }
+    }
+
+    pub fn len(&self) -> usize { self.bits.len() }
+
+    pub fn new() -> BitString {
+        BitString {bits: BitVec::new()}
+    }
+
+    pub fn from_elem(len: usize, val: bool) -> BitString {
+        BitString {bits: BitVec::from_elem(len, val)}
+    }
+
+    pub fn to_vec(&self) -> Vec<bool> {
+        self.bits.iter().collect()
+    }
+}
+
+#[test]
+fn test_bitstring() {
+    let bs = BitString::new();
+    assert_eq!(0, bs.len());
+
+    let bs = BitString::from_elem(4, true);
+    assert_eq!(4, bs.len());
+    assert_eq!(vec!(true, true, true, true), bs.to_vec());
+
+    let mut bs = BitString::new();
+    bs.append_bits_msb_from(&0b11001, 4);
+    assert_eq!(4, bs.len());
+    assert_eq!(vec!(true, false, false, true), bs.to_vec());
+}
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+#![feature(num_bits_bytes)]
+
+extern crate bit_vec;
+pub mod bit_string;
