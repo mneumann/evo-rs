6d15b00009c0f6aec66bf7b0eb98cfd85868bd21
Michael Neumann <mneumann@ntecs.de>
2015-11-29 18:19:04 +0100

    Move tournament_selection into selection module.

---
+++ src/selection.rs
@@ -0,0 +1,33 @@
+use rand::Rng;
+
+/// Select the best individual out of `k` randomly choosen.
+/// This gives individuals with better fitness a higher chance to reproduce.
+/// `n` is the total number of individuals.
+///
+/// NOTE: We are not using `sample(rng, 0..n, k)` as it is *very* expensive.
+/// Instead we call `rng.gen_range()` k-times. The drawn items could be the same,
+/// but the probability is very low if `n` is high compared to `k`.
+#[inline]
+pub fn tournament_selection_fast<R: Rng, F>(rng: &mut R,
+                                          better_than: F,
+                                          n: usize,
+                                          k: usize)
+                                          -> usize
+    where F: Fn(usize, usize) -> bool
+{
+    assert!(n > 0);
+    assert!(k > 0);
+    assert!(n >= k);
+
+    let mut best: usize = rng.gen_range(0, n);
+
+    for _ in 1..k {
+        let i = rng.gen_range(0, n);
+        if better_than(i, best) {
+            best = i;
+        }
+    }
+
+    return best;
+}
+
